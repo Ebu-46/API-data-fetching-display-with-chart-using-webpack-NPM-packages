014989e14c2bf49949597ef1f27ebc12104881be
Md.Zahid Hasan Ebu <30809719+Ebu-46@users.noreply.github.com>
2021-04-18 11:47:25 +0600

    without webpack

---
+++ chart.js
@@ -0,0 +1,31 @@
+apiData().then(cities => {
+    let html = '<h2> Temperature Chart</h2>';
+    let body = document.getElementById("body");
+    for (let i = 0; i < cities.length; i++){
+        console.log(cities[i]);
+        
+        html += `<div class="progress-bar">
+                    ${cities[i].name}
+                    <div class="progress-track">
+                    <div class="progress-fill">
+                        <span class="bar">${Math.round(cities[i].main.temp)-273}C</span>
+                    </div>
+                    </div>
+                </div>`;
+    }
+    body.innerHTML = html;
+
+    let x = document.getElementsByClassName("progress-fill");
+    let y = document.getElementsByClassName("bar");
+    for (let i = 0; i < x.length; i++) {
+        let percent = y[i].innerHTML;
+        console.log(percent.slice(0, percent.length - 1));
+        let pTop = 100 - parseInt( percent.slice(0, percent.length - 1));
+        
+        x[i].style.height = 100-pTop + "%";
+        x[i].style.top = pTop + "%";
+        
+    }
+});
+
+
---
+++ main.js
@@ -0,0 +1,22 @@
+async function apiData(){
+    let cities=[];
+    let response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=b0dfd5e61ca1609f0a1eb7f74247e03e');
+    cities.push(await response.json());
+    
+    // fetch('http://api.openweathermap.org/data/2.5/weather?q=dhaka&appid=b0dfd5e61ca1609f0a1eb7f74247e03e')
+    // .then(response => response.json())
+    // .then(data => cities.push(data));
+
+    response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=rajshahi&appid=b0dfd5e61ca1609f0a1eb7f74247e03e');
+    cities.push(await response.json());
+   
+
+    response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=khulna&appid=b0dfd5e61ca1609f0a1eb7f74247e03e');
+    cities.push(await response.json());
+
+
+    return cities;
+}
+
+
+
